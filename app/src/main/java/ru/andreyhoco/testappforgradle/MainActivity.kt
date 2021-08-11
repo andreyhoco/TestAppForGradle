e8ca59ec88e93d2546302310b7ad8c9b81eff26c
andreyhoco <andreygud2013@gmail.com>
2021-08-11 14:57:53 +0300

    Add editorconfig

---
+++ app/src/main/java/ru/andreyhoco/testappforgradle/MainActivity.kt
@@ -0,0 +1,19 @@
+package ru.andreyhoco.testappforgradle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("onStart", "start")
+    }
+}
